efa34ee6959825815246667dac992c2b44bfd175
YanaMarkova <yana_markova@yahoo.com>
2023-01-11 17:26:13 +0100

    Initital commit

---
+++ Syntax/stringLength.js
@@ -0,0 +1,10 @@
+function strLength (a, b, c){
+
+    let total = a.length + b.length + c.length;
+
+    console.log(total);
+    console.log(Math.floor(total / 3));
+}
+
+strLength('chocolate', 'ice cream', 'cake');
+strLength('pasta', '5', '22.3');
