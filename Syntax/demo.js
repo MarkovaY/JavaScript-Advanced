3e21b49a345d4c4516c8b58184c95d48bba64110
YanaMarkova <yana_markova@yahoo.com>
2023-01-11 16:35:16 +0100

    Initial commit

--- Syntax/demo.js
+++ Syntax/demo.js
@@ -1,6 +1,18 @@
+//That's just a demo, while learning the basics of JS
+
 console.log('Hello world!');
 
 let a = 5;
 let b = 3;
 
 console.log(a + b);
+
+// Functions in JS
+
+function values (a, b) {
+    console.log(a);
+    console.log(b);
+}
+
+values (5, 7)
+
