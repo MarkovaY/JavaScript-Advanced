f23afe11df31b745bcfa9f0f976efdf2b62ecdf7
YanaMarkova <yana_markova@yahoo.com>
2023-01-22 12:22:04 +0100

    Initial commit

--- Objects/cityRecord.js
+++ Objects/cityRecord.js
@@ -1,6 +1,7 @@
 // You will receive a city’s name, population, and treasury as arguments, 
 // which you will need to set as properties of an object and return it.
 
+// @ts-check
 function city(name, population, treasury) {
     const city = {
         name,
